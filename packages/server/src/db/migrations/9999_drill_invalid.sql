f2221ad4a3149c75416b76a9ff5618f577e89e72
testProject <frankwaster@hotmail.com>
2026-05-30 11:12:57 +0800

    Revert "DRILL: invalid migration to test deploy-agent migrate-abort (revert after)"
    This reverts commit ca0a40e57b168e06bbd57e916284a2abb805eef5.

--- packages/server/src/db/migrations/9999_drill_invalid.sql
+++
@@ -1,6 +0,0 @@
--- DRILL: intentionally invalid migration to test deploy-agent migrate-abort path.
--- Expected: db:migrate fails, deploy-agent aborts before restart, old service keeps serving.
--- This will be reverted immediately after the drill.
-CREATE TABLE drill_invalid (
-  id integer PRIMARY KEY
-) THIS IS NOT VALID SQL;
